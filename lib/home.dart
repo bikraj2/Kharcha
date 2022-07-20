3f16d811bd107be20c70941d96da77b09868a741
1shan-pixel <ispanta03@gmail.com>
2022-07-20 23:18:26 +0545

    bottom navbar added to home

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'main.dart' ; 
+import 'package:flutter/cupertino.dart';
+
 
 class HomeScreen extends StatefulWidget {
  HomeScreen({Key? key}) : super(key: key);
@@ -15,6 +17,30 @@ class _HomeScreenState extends State <HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"), 
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true, 
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), 
+            activeIcon: Icon(Icons.check),
+            label: 'Home', 
+          
+             
+          ), 
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map), 
+            label: 'Chart', 
+          ), 
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person), 
+            label: 'Profile', 
+          ), 
+          
+
+
+        ],
       ),
     ); 
   }
