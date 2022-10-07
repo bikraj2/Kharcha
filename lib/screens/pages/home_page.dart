6ce0a8290e412e360cd86b21f49da81788e0d52a
PriyanshuSharma1 <psneb420@gmail.com>
2022-10-07 22:43:43 +0545

    homepage updated

--- lib/screens/pages/home_page.dart
+++ lib/screens/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo2/screens/log/login_screen.dart';
 import 'package:demo2/screens/pages/add_expenses.dart';
 import 'package:demo2/theme/theme.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +13,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 35, 45, 64),
+        title: Text(
+          'Hi Username!',
+          style: TextStyle(fontStyle: FontStyle.italic, fontSize: 23),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.person),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+        ),
+      ),
       body: Container(
           padding: EdgeInsets.all(20),
           alignment: Alignment.bottomRight,
           child: CircleAvatar(
             backgroundColor: AppTheme.colors.basecolor,
-            radius: 30,
+            radius: 25,
             child: IconButton(
               onPressed: () {
                 Navigator.push(
