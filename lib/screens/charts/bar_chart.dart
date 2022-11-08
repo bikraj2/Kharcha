7e70e4185fc873f58577ebdfd79fdcab45a7365d
1shan-pixel <ispanta03@gmail.com>
2022-11-08 18:09:25 +0545

    bar touch data removed from bar chart

--- lib/screens/charts/bar_chart.dart
+++ lib/screens/charts/bar_chart.dart
@@ -27,7 +27,7 @@ class BarChartSample1 extends StatefulWidget {
 
 class BarChartSample1State extends State<BarChartSample1> {
   List<Map<String, Object>> value1 = expenseList.groupedData;
-  
+
   final Color barBackgroundColor = const Color(0xff72d8bf);
   final Duration animDuration = const Duration(milliseconds: 250);
   bool isLoading = true;
@@ -153,68 +153,6 @@ class BarChartSample1State extends State<BarChartSample1> {
 
   BarChartData mainBarData() {
     return BarChartData(
-      barTouchData: BarTouchData(
-        touchTooltipData: BarTouchTooltipData(
-          tooltipBgColor: Color.fromARGB(255, 201, 138, 90),
-          getTooltipItem: (group, groupIndex, rod, rodIndex) {
-            String weekDay;
-            switch (group.x) {
-              case 0:
-                weekDay = 'Sunday';
-                break;
-              case 1:
-                weekDay = 'Monday';
-                break;
-              case 2:
-                weekDay = 'Tuesday';
-                break;
-              case 3:
-                weekDay = 'Wednesday';
-                break;
-              case 4:
-                weekDay = 'Thursday';
-                break;
-              case 5:
-                weekDay = 'Friday';
-                break;
-              case 6:
-                weekDay = 'Saturday';
-                break;
-              default:
-                throw Error();
-            }
-            return BarTooltipItem(
-              '$weekDay\n',
-              const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                  text: (rod.toY - 1).toString(),
-                  style: const TextStyle(
-                    color: Colors.yellow,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-        touchCallback: (FlTouchEvent event, barTouchResponse) {
-          setState(() {
-            if (!event.isInterestedForInteractions ||
-                barTouchResponse == null ||
-                barTouchResponse.spot == null) {
-              touchedIndex = -1;
-              return;
-            }
-            touchedIndex = barTouchResponse.spot!.touchedBarGroupIndex;
-          });
-        },
-      ),
       titlesData: FlTitlesData(
         show: true,
         rightTitles: AxisTitles(
