cedec4fb66f8d515fbf36388ddf65612ed17e9c0
PriyanshuSharma1 <psneb420@gmail.com>
2022-11-09 21:45:50 +0545

    ui almost completed

--- lib/screens/charts/graphs_page.dart
+++ lib/screens/charts/graphs_page.dart
@@ -24,90 +24,101 @@ class _GraphPageState extends State<GraphPage> {
     width = size.width;
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisSize: MainAxisSize.max,
-          verticalDirection: VerticalDirection.down,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Row(
-              children: [
-                Container(
-                  color: Colors.white,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (BuildContext) {
-                        return PieChartPage();
-                      }));
-                    },
-                    icon: Image(image: AssetImage("assetss/images/pie.png")),
-                    iconSize: width / 2.5,
-                    // child: Container(
-                    //     height: height / 4.5,
-                    //     width: width / 3,
-                    //     decoration: const BoxDecoration(
-                    //         color: Colors.white,
-                    //         image: DecorationImage(
-                    //           image: AssetImage('assetss/images/pie.png'),
-                    //           fit: BoxFit.cover,
-                    //         ))),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            verticalDirection: VerticalDirection.down,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      color: Colors.white,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (BuildContext) {
+                            return PieChartPage();
+                          }));
+                        },
+                        icon: Icon(Icons.pie_chart_rounded),
+                        iconSize: width / 2.5,
+                        color: AppTheme.colors.secondarycolor,
+                      )
+                      // child: Container(
+                      //     height: height / 4.5,
+                      //     width: width / 3,
+                      //     decoration: const BoxDecoration(
+                      //         color: Colors.white,
+                      //         image: DecorationImage(
+                      //           image: AssetImage('assetss/images/pie.png'),
+                      //           fit: BoxFit.cover,
+                      //         ))),
+                      ),
+                  Container(
+                    child: Text(
+                        "\n Something about pie chart \n i really love pie chart "),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    height: height / 5,
+                    width: width / 1.8,
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  height: height / 5,
-                  width: width / 1.8,
-                ),
+                  Container(
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (BuildContext) {
+                          return LineChartClass1();
+                        }));
+                      },
+                      icon: Icon(Icons.stacked_line_chart_rounded),
+                      color: AppTheme.colors.basecolor,
+                      iconSize: width / 2.5,
+                      // child: Container(
+                      //     height: height / 4.5,
+                      //     width: width / 4,
+                      //     decoration: const BoxDecoration(
+                      //         image: DecorationImage(
+                      //             image: AssetImage('assetss/images/chart.png'),
+                      //             fit: BoxFit.cover)))
+                    ),
+                  ),
+                ],
+              ),
+              Row(children: [
                 Container(
                   child: IconButton(
                     onPressed: () {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (BuildContext) {
-                        return LineChartClass1();
+                        return BarChartSample1();
                       }));
                     },
-                    icon: Image(image: AssetImage("assetss/images/chart.png")),
+                    icon: Icon(Icons.stacked_bar_chart_rounded),
+                    color: AppTheme.colors.secondarycolor,
                     iconSize: width / 2.5,
                     // child: Container(
                     //     height: height / 4.5,
                     //     width: width / 4,
                     //     decoration: const BoxDecoration(
                     //         image: DecorationImage(
-                    //             image: AssetImage('assetss/images/chart.png'),
+                    //             image: AssetImage('assetss/images/pie.png'),
                     //             fit: BoxFit.cover)))
                   ),
-                ),
-              ],
-            ),
-            Row(children: [
-              Container(
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (BuildContext) {
-                      return BarChartSample1();
-                    }));
-                  },
-                  icon: Image(image: AssetImage("assetss/images/pie.png")),
-                  iconSize: width / 2.5,
-                  // child: Container(
-                  //     height: height / 4.5,
-                  //     width: width / 4,
-                  //     decoration: const BoxDecoration(
-                  //         image: DecorationImage(
-                  //             image: AssetImage('assetss/images/pie.png'),
-                  //             fit: BoxFit.cover)))
-                ),
-              )
-            ]),
-          ],
+                )
+              ]),
+            ],
+          ),
         ));
   }
 }
