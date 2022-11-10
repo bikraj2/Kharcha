19b0547a74428a975c7e9fe3a6f95974c4a3c1a2
PriyanshuSharma1 <psneb420@gmail.com>
2022-11-10 09:24:14 +0545

    add expenses fully enhanced

--- lib/screens/pages/add_expenses.dart
+++ lib/screens/pages/add_expenses.dart
@@ -6,6 +6,7 @@ import 'package:demo2/screens/pages/home_page.dart';
 import 'package:demo2/services/authservices.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 import '../../theme/theme.dart';
 import '../../token/token.dart';
 import 'package:demo2/models/expenseList.dart';
@@ -56,155 +57,166 @@ class _expenseAdderState extends State<expenseAdder> {
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat("EEE d MMM").format(now);
     orientation = MediaQuery.of(context).orientation;
     //size of the window
     size = MediaQuery.of(context).size;
     height = size.height;
     width = size.width;
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: AppTheme.colors.basecolor,
-          title: Text(
-            ' Add Expenses!',
-            style:
-                TextStyle(fontStyle: FontStyle.italic, fontSize: height / 30),
-          ),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => HomePage()));
-            },
-          ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppTheme.colors.basecolor,
+        title: Text(
+          ' Add Expenses!',
+          style: TextStyle(fontStyle: FontStyle.italic, fontSize: height / 30),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                      width: width / 1.5,
-                      height: height / 8,
-                      padding: EdgeInsets.all(15),
-                      margin: EdgeInsets.only(top: 50),
-                      child: TextField(
-                          controller: _moneyController,
-                          decoration: InputDecoration(
-                              labelText: 'Amount ',
-                              border: OutlineInputBorder(),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  _moneyController.clear();
-                                },
-                                icon: Icon(Icons.clear),
-                              )))),
-                  Container(
-                      margin: EdgeInsets.only(top: 10),
-                      width: width / 1.5,
-                      height: height / 8,
-                      padding: EdgeInsets.all(15),
-                      child: TextField(
-                          controller: _dateController,
-                          decoration: InputDecoration(
-                              labelText: 'Expense Title ',
-                              border: OutlineInputBorder(),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  _dateController.clear();
-                                },
-                                icon: Icon(Icons.clear),
-                              ))))
-                ],
-              ),
-            ),
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(15),
-                margin: EdgeInsets.all(10),
-                width: width / 1.5,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                child: DropdownButton<String>(
-                  value: value,
-                  isExpanded: true,
-                  iconSize: 30,
-                  icon: Icon(Icons.arrow_drop_down, color: Colors.black),
-                  items: category.map(buildMenuItem).toList(),
-                  onChanged: (value) => setState(() {
-                    this.value = value;
-                  }),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  "Today, " + formattedDate,
+                  style: TextStyle(
+                    color: AppTheme.colors.secondarycolor,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
-
-            Center(
-              child: IconButton(
-                color: Theme.of(context).primaryColor,
-                hoverColor: Theme.of(context).bottomAppBarColor,
-                icon: Icon(Icons.calendar_month_outlined),
-                onPressed: () {
-                  getDate();
-                },
-              ),
-            ),
-            Center(
-              child: Container(
+            ],
+          ),
+          Center(
+            child: Column(
+              children: [
+                Container(
+                    width: width,
+                    height: height / 8,
+                    padding: EdgeInsets.all(15),
+                    margin: EdgeInsets.only(top: 10),
+                    child: TextField(
+                        controller: _moneyController,
+                        decoration: InputDecoration(
+                            labelText: 'Amount ',
+                            border: OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                _moneyController.clear();
+                              },
+                              icon: Icon(Icons.clear),
+                              iconSize: 14,
+                            )))),
+                Container(
+                    margin: EdgeInsets.only(top: 10),
+                    width: width,
+                    height: height / 8,
+                    padding: EdgeInsets.all(15),
+                    child: TextField(
+                        controller: _dateController,
+                        decoration: InputDecoration(
+                            labelText: 'Expense Title ',
+                            border: OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                _dateController.clear();
+                              },
+                              icon: Icon(Icons.clear),
+                              iconSize: 14,
+                            )))),
+                Container(
                   padding: EdgeInsets.all(15),
-                  child: FloatingActionButton(
-                      shape: BeveledRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5))),
-                      hoverColor: AppTheme.colors.basecolor,
-                      backgroundColor: AppTheme.colors.secondarycolor,
-                      child: const Text("Save"),
-                      onPressed: () {
-                        setState(() {
-                          date = _dateController.text;
-                          money = double.parse(_moneyController.text);
-                          categories = value;
-                        });
-                        var expense = Expense(
-                            name: _dateController.text,
-                            category: categories.toString(),
-                            amount: money,
-                            date1: currentDate);
-
-                        token.storage.read(key: "jwt").then((value) {
-                          AuthService().addexpense(expense, value).then((val) {
-                            if (val.data['success']) {
-                              Fluttertoast.showToast(
-                                  msg: val.data['msg'],
-                                  textColor: Colors.white,
-                                  backgroundColor: Colors.green);
-                            } else {
-                              Fluttertoast.showToast(
-                                  msg: val.data['msg'],
-                                  textColor: Colors.white,
-                                  backgroundColor: Colors.green);
-                            }
+                  margin: EdgeInsets.all(10),
+                  width: width / 1.2,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: DropdownButton<String>(
+                    value: value,
+                    isExpanded: true,
+                    iconSize: 30,
+                    icon: Icon(Icons.arrow_drop_down,
+                        color: AppTheme.colors.secondarycolor),
+                    items: category.map(buildMenuItem).toList(),
+                    onChanged: (value) => setState(() {
+                      this.value = value;
+                    }),
+                  ),
+                ),
+                IconButton(
+                  color: Theme.of(context).primaryColor,
+                  hoverColor: Theme.of(context).bottomAppBarColor,
+                  icon: Icon(
+                    Icons.calendar_month_outlined,
+                    color: AppTheme.colors.secondarycolor,
+                  ),
+                  iconSize: 35,
+                  onPressed: () {
+                    getDate();
+                  },
+                ),
+                Container(
+                    padding: EdgeInsets.all(15),
+                    child: ElevatedButton(
+                        style: ButtonStyle(),
+                        // shape: BeveledRectangleBorder(
+                        //     borderRadius: BorderRadius.all(Radius.circular(5))),
+                        // hoverColor: AppTheme.colors.basecolor,
+                        // backgroundColor: AppTheme.colors.secondarycolor,
+                        child: const Text(
+                          "Save",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w600),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            date = _dateController.text;
+                            money = double.parse(_moneyController.text);
+                            categories = value;
                           });
-                        });
-                      })),
+                          var expense = Expense(
+                              name: _dateController.text,
+                              category: categories.toString(),
+                              amount: money,
+                              date1: currentDate);
+
+                          token.storage.read(key: "jwt").then((value) {
+                            AuthService()
+                                .addexpense(expense, value)
+                                .then((val) {
+                              if (val.data['success']) {
+                                Fluttertoast.showToast(
+                                    msg: val.data['msg'],
+                                    textColor: Colors.white,
+                                    backgroundColor: Colors.green);
+                              } else {
+                                Fluttertoast.showToast(
+                                    msg: val.data['msg'],
+                                    textColor: Colors.white,
+                                    backgroundColor: Colors.green);
+                              }
+                            });
+                          });
+                        })),
+              ],
             ),
-            // Center(
-            //   child: Container(
-            //       padding: EdgeInsets.all(25),
-            //       child: FloatingActionButton(
-            //           child: Text("Pie"),
-            //           onPressed: () {
-            //             Navigator.push(context, MaterialPageRoute(
-            //               builder: (BuildContext context) {
-            //                 return Piechart();
-            //               },
-            //             ));
-            //           })),
-            // ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
